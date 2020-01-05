255c7b3ea1a1a03e482dcac68df2d4d242a0ba7f
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-05 19:12:49 +0400

    Add Vuex-Persist to Vuex store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,29 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { StoreOptions } from "vuex";
+
+import {RootState} from './types';
+// Use VuexPersist
+import VuexPersist from 'vuex-persist';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+const vuexLocalStorage = new VuexPersist<RootState>({
+  storage: window.localStorage,
+  reducer: (state) => ({
+    // Add stuff here
+  })
+})
+
+const store: StoreOptions<RootState> = {
+  state: {
+    version: '0.0.1'
+  },
+  modules: {
+    // Register modules
+  },
+  plugins: [
+    vuexLocalStorage.plugin,
+  ]
+}
+
+export default new Vuex.Store<RootState>(store);
