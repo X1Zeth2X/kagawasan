5495536c101bac527ec3748d2bc1633f1706f590
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-06 14:24:04 +0400

    Add mock home components.

---
+++ src/components/Home/Composer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="composer">
+    <v-card class="c-main" hover>
+      <v-textarea rows="2" auto-grow solo-inverted></v-textarea>
+
+      <v-card-actions>
+        <v-btn-toggle v-model="mode" rounded>
+          <v-btn>
+            <v-icon>ion-ios-create</v-icon>
+          </v-btn>
+          <v-btn>
+            <v-icon>ion-ios-image</v-icon>
+          </v-btn>
+          <v-btn>
+            <v-icon>ion-ios-aperture</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+        <v-spacer></v-spacer>
+
+        <v-btn fab small color="teal">
+          <v-icon>
+            ion-ios-send
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Composer extends Vue {
+  private mode: number = 0;
+}
+</script>
+
+<style lang="scss">
+#composer {
+  .c-main {
+    border-radius: 0.2em 0.2em 2em 2em;
+  }
+}
+</style>
