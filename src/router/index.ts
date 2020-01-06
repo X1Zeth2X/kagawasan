26bb0a6ee02293f29d60db1973a4903ec68d2400
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-06 14:20:37 +0400

    Add settings route and use lazy imports.

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,18 +9,27 @@ const routes = [
   {
     path: "/",
     name: "home",
-    // component: () => import('@/views/Home.vue'),
-    component: Home
+    component: () => import("@/views/Home.vue"),
+    meta: {
+      showNav: true
+    }
   },
   {
     path: "/auth",
     name: "auth",
-    // component: () => import('@/views/Home.vue'),
     component: Auth,
     meta: {
       public: true,
       onlyWhenLoggedOut: true,
       showNav: false
+    }
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: () => import("@/views/Settings.vue"),
+    meta: {
+      showNav: true
     }
   }
 ];
