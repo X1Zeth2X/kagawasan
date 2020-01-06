5fa14f953fbf54ae2eb0bd00a86d1e60254949d6
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-06 14:22:11 +0400

    Add settings view.

---
+++ src/views/Settings.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="settingsView">
+    <v-container>
+      <v-row align="start" justify="start">
+        <v-col cols="12">
+          <div class="f1 fw7 mb4">
+            Settings
+          </div>
+
+          <Layout />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import Layout from "@/components/Settings/Layout.vue";
+
+@Component({
+  components: {
+    Layout
+  }
+})
+export default class Settings extends Vue {}
+</script>
