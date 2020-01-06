945b6d924af96234fa97d06fe850dd1ee7d7cb08
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-07 03:26:12 +0400

    Created CurrentUser component.

--- src/components/Nav/Drawer/CurrentUser.vue
+++ src/components/Nav/Drawer/CurrentUser.vue
@@ -1,4 +1,21 @@
-<template> </template>
+<template>
+  <v-list-item two-line>
+    <v-list-item-avatar>
+      <img src="https://randomuser.me/api/portraits/lego/6.jpg" />
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title>X1Zeth2X</v-list-item-title>
+      <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+    </v-list-item-content>
+
+    <v-list-item-action>
+      <v-btn icon>
+        <v-icon>ion-md-more</v-icon>
+      </v-btn>
+    </v-list-item-action>
+  </v-list-item>
+</template>
 
 <script lang="ts">
 import Vue from "vue";
