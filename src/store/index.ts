2dc87f0d1aa2247351768a5d26447a6a7b6df55b
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-06 14:20:04 +0400

    Register layout module.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,29 +1,30 @@
 import Vue from "vue";
 import Vuex, { StoreOptions } from "vuex";
 
-import {RootState} from './types';
+import { RootState } from "./types";
 // Use VuexPersist
-import VuexPersist from 'vuex-persist';
+import VuexPersist from "vuex-persist";
+
+import { layout } from './modules/layout/index';
 
 Vue.use(Vuex);
 
 const vuexLocalStorage = new VuexPersist<RootState>({
   storage: window.localStorage,
-  reducer: (state) => ({
+  reducer: state => ({
     // Add stuff here
   })
-})
+});
 
 const store: StoreOptions<RootState> = {
   state: {
-    version: '0.0.1'
+    version: "0.0.1"
   },
   modules: {
     // Register modules
+    layout
   },
-  plugins: [
-    vuexLocalStorage.plugin,
-  ]
-}
+  plugins: [vuexLocalStorage.plugin]
+};
 
 export default new Vuex.Store<RootState>(store);
