537774ad783750170342dbb0af99ef87e58bc02d
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-06 14:23:38 +0400

    Added mock home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,31 @@
 <template>
-  <div class="home">
-    Test
+  <div id="homeView">
+    <v-container>
+      <v-row align="start" justify="space-around">
+        <v-col md="7">
+          <div class="f3 fw7">Create</div>
+          <p>What's on your mind?</p>
+          <Composer />
+
+          <v-divider class="mv4"></v-divider>
+
+          <PostMain />
+        </v-col>
+        <v-col md="4">
+          <div class="f2 fw2">Planning Board</div>
+          <div class="f3 fw7">Standard Layout</div>
+          <p>
+            Your usual social platform layout where the composer and feed is in
+            the middle, and other stuff on the right side, navigation drawer.
+          </p>
+
+          <div class="f3 fw7">Masonry Layout</div>
+          <p>
+            Composer is a dialog and posts are shown in a masonry layout.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -8,6 +33,14 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
-@Component
+import Composer from "@/components/Home/Composer.vue";
+import PostMain from "@/components/Home/Post/PostMain.vue";
+
+@Component({
+  components: {
+    PostMain,
+    Composer
+  }
+})
 export default class Home extends Vue {}
 </script>
