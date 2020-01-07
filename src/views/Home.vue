9d864e60fb34f6b516600d25e6c487821896153b
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-07 11:05:42 +0400

    Allow switching from MasonryLayout to StandardLayout.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,8 @@
 <template>
   <div id="homeView">
-    <v-container>
-      <v-row align="start" justify="space-around">
-        <v-col md="7">
-          <div class="f3 fw7">Create</div>
-          <p>What's on your mind?</p>
-          <Composer />
-
-          <v-divider class="mv4"></v-divider>
-
-          <PostMain />
-        </v-col>
-        <v-col md="4">
-          <div class="f2 fw2">Planning Board</div>
-          <div class="f3 fw7">Standard Layout</div>
-          <p>
-            Your usual social platform layout where the composer and feed is in
-            the middle, and other stuff on the right side, navigation drawer.
-          </p>
-
-          <div class="f3 fw7">Masonry Layout</div>
-          <p>
-            Composer is a dialog and posts are shown in a masonry layout.
-          </p>
-        </v-col>
-      </v-row>
+    <v-container fluid>
+      <MasonryLayout v-if="masonryLayout"/>
+      <StandardLayout v-else/>
     </v-container>
   </div>
 </template>
@@ -32,15 +10,21 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { Getter } from "vuex-class";
 
-import Composer from "@/components/Home/Composer.vue";
-import PostMain from "@/components/Home/Post/PostMain.vue";
+import StandardLayout from "@/components/Home/StandardLayout.vue";
+import MasonryLayout from "@/components/Home/MasonryLayout.vue";
+import ComposerDialog from "@/components/Home/ComposerDialog.vue";
 
 @Component({
   components: {
-    PostMain,
-    Composer
+    StandardLayout,
+    MasonryLayout,
+    ComposerDialog
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Getter("masonryLayout", { namespace: "themeLayout" })
+  private masonryLayout!: boolean;
+}
 </script>
