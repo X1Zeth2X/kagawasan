1e4827b3e36dbe7734c83841c2791367cb6e516b
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-07 13:07:51 +0400

    Add content settings.

---
+++ src/components/Settings/Content.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header
+      expand-icon="ion-ios-arrow-down"
+    >
+      CONTENT
+    </v-expansion-panel-header>
+
+    <v-expansion-panel-content>
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index">
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ item.subtitle }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-switch
+              inset
+              v-model="item.value"
+              @change="item.onChange"
+            ></v-switch>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Getter, Action } from "vuex-class";
+
+const namespace: string = "themeLayout";
+
+interface Option {
+  title: string;
+  subtitle: string;
+  value: boolean;
+  onChange: Function;
+}
+
+@Component
+export default class Layout extends Vue {
+
+  private items: Option[] = [
+    {
+      title: "Markdown",
+      subtitle: "Markdown syntax on contents. (No truncation/limit, this may cause long boi posts.)",
+      value: this.$vuetify.theme.dark,
+      onChange: () => alert("implement me"),
+    }
+  ];
+}
+</script>
+
