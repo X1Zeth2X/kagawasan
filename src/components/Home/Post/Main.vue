17bb0379edef59f8279e2fb2412272fc07f3f4e9
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-07 11:04:34 +0400

    Add Post components.

---
+++ src/components/Home/Post/Main.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card id="post" class="mb2">
+    <PostTop />
+
+    <v-card-text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores
+      rerum, laudantium perspiciatis ab ratione fugit pariatur explicabo.
+      Ducimus dolor eligendi dolorum quaerat corrupti aspernatur deleniti
+      perferendis voluptatum fugit earum.
+    </v-card-text>
+    <!-- Post content -->
+
+    <v-img src="https://source.unsplash.com/1600x900/?nature,water"></v-img>
+    <!-- Post Image -->
+
+    <PostActions />
+    <!-- Post Actions (Like, Comment, ...) -->
+
+    <v-divider></v-divider>
+
+    <div>
+      <Comment />
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import PostTop from "./Top.vue";
+import PostActions from "./Actions.vue";
+
+@Component({
+  components: {
+    PostTop,
+    PostActions,
+    Comment: () => import("./Comment/Main.vue")
+  }
+})
+export default class PostMain extends Vue {}
+</script>
+
+<style lang="scss">
+#post {
+  border-radius: 1em;
+}
+</style>
---
+++ src/components/Home/Post/Top.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-list-item>
+    <v-list-item-avatar>
+      <a href="#">
+        <v-avatar size="45" class="primary">
+          <v-icon dark>ion-md-person</v-icon>
+        </v-avatar>
+      </a>
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title class="b">
+        Zeth Leonardo
+      </v-list-item-title>
+
+      <v-list-item-subtitle>
+        29th Jan, 2020
+      </v-list-item-subtitle>
+    </v-list-item-content>
+
+    <v-list-item-icon>
+      <v-menu transition="scale-transition">
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" icon>
+            <v-icon>ion-md-more</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            Edit
+          </v-list-item>
+
+          <v-list-item>
+            Delete
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-list-item-icon>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class PostTop extends Vue {}
+</script>
