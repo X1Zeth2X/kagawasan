40206ea616716340c962d3d0f1220d92943d3486
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-08 15:50:25 +0400

    Added base ProfileView component.

---
+++ src/views/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="profileView">
+    Profile View
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Profile extends Vue {}
+</script>
