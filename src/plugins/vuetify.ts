f00f94968c7366d91b72cb2a59e2d0db5debcdaa
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-08 15:35:26 +0400

    Sync dark mode setting with Vuex.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import store from "@/store";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-    dark: true
+    dark: store.getters["darkMode"]
   }
 });
