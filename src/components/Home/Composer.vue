74ab0d4dd4e99bd131f32050df59b50175595573
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-08 15:32:37 +0400

    Decoupled Composer.

--- src/components/Home/Composer.vue
+++ src/components/Home/Composer.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="composer">
     <v-card class="c-main" hover>
-      <v-textarea
-        rows="2"
-        auto-grow
-        :solo-inverted="$vuetify.theme.dark"
-        :solo="!$vuetify.theme.dark"
-      ></v-textarea>
+      <v-window v-model="mode">
+        <Writer v-on:update="syncContent" />
+        <Uploader />
+      </v-window>
 
       <v-card-actions>
-        <v-btn-toggle v-model="mode" rounded>
-          <v-btn>
-            <v-icon>ion-ios-create</v-icon>
-          </v-btn>
-          <v-btn>
-            <v-icon>ion-ios-image</v-icon>
-          </v-btn>
-          <v-btn>
-            <v-icon>ion-ios-aperture</v-icon>
-          </v-btn>
+        <v-btn-toggle v-model="mode" rounded color="primary">
+          <v-tooltip
+            v-for="(composerMode, index) in composerModes"
+            :key="index"
+            bottom
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn v-on="on">
+                <v-icon>{{ composerMode.icon }}</v-icon>
+              </v-btn>
+            </template>
+            {{ composerMode.desc }}
+          </v-tooltip>
         </v-btn-toggle>
         <v-spacer></v-spacer>
 
-        <v-btn fab small color="teal">
+        <v-btn fab small color="teal" dark>
           <v-icon>
             ion-ios-send
           </v-icon>
@@ -36,9 +37,48 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
-@Component
+import Writer from "./ComposerItems/Writer.vue";
+
+interface PostData {
+  content: string;
+  imageId: string;
+}
+
+@Component({
+  components: {
+    Writer,
+    /*
+      Lazy import uploader since it
+      is going to be a large component.
+    */
+    Uploader: () => import("./ComposerItems/Uploader.vue")
+  }
+})
 export default class Composer extends Vue {
   private mode: number = 0;
+  private postData: PostData = {
+    content: "",
+    imageId: ""
+  };
+
+  private composerModes: object[] = [
+    {
+      desc: "Write a post",
+      icon: "ion-ios-create"
+    },
+    {
+      desc: "Upload an image",
+      icon: "ion-ios-image"
+    },
+    {
+      desc: "Post settings",
+      icon: "ion-ios-aperture"
+    }
+  ];
+
+  private syncContent(content: string) {
+    this.postData.content = content;
+  }
 }
 </script>
 
