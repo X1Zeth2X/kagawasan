2d5ce01abff7cf67b6f2ca91f0ddc53a138975a6
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-08 15:33:05 +0400

    Added Composer components.

---
+++ src/components/Home/ComposerItems/Uploader.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-window-item>
+    <v-card>
+      <v-card-text>
+        Implement Uploader
+      </v-card-text>
+    </v-card>
+  </v-window-item>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Uploader extends Vue {}
+</script>
---
+++ src/components/Home/ComposerItems/Writer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-window-item>
+    <v-textarea
+      rows="2"
+      auto-grow
+      :solo-inverted="$vuetify.theme.dark"
+      :solo="!$vuetify.theme.dark"
+      v-model="content"
+      @keyup="update(content)"
+    ></v-textarea>
+  </v-window-item>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop, Emit } from "vue-property-decorator";
+
+@Component
+export default class Writer extends Vue {
+  private content: string = "";
+
+  @Emit("update")
+  private update(content: string): string {
+    return content;
+  }
+}
+</script>
