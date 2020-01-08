7d4525d3f4a1b3db4fa1703be35c667ae2f385d1
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-08 15:32:16 +0400

    Declared VueMarkdown and VueTruncateCollapsed modules.

--- src/vendor.d.ts
+++ src/vendor.d.ts
@@ -1,3 +1,7 @@
 // Declare the full vee-validate package.
 declare module "vee-validate/dist/vee-validate.full";
 declare module "vue-masonry-css";
+
+// Declare markdown and truncate plugin
+declare module "vue-markdown";
+declare module "vue-truncate-collapsed";
