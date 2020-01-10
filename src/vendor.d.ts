3a232e47f603a1f0e52294529804fafa0e97c164
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-10 15:23:26 +0400

    Remove vue-masonry-css

--- src/vendor.d.ts
+++ src/vendor.d.ts
@@ -1,6 +1,5 @@
 // Declare the full vee-validate package.
 declare module "vee-validate/dist/vee-validate.full";
-declare module "vue-masonry-css";
 
 // Declare markdown and truncate plugin
 declare module "vue-markdown";
