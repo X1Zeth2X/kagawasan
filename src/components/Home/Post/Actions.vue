6eb9a20388d6276a077ca298d2b74a0272425709
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-18 03:20:59 +0400

    Added functionality.

--- src/components/Home/Post/Actions.vue
+++ src/components/Home/Post/Actions.vue
@@ -1,16 +1,16 @@
 <template>
   <v-card-actions>
     <v-btn icon @click="toggleKek">
-      <v-icon :color="kekGiven ? 'red lighten-1' : ''">
-        {{ kekGiven ? "ion-ios-heart" : "ion-ios-heart-empty" }}
+      <v-icon :color="action.kekGiven ? 'red lighten-1' : ''">
+        {{ action.kekGiven ? "ion-ios-heart" : "ion-ios-heart-empty" }}
       </v-icon>
     </v-btn>
-    <span class="mb1 f6">19k</span>
+    <span class="mb1 f6">{{ action.likes }}</span>
 
-    <v-btn icon @click="$emit('toggleCommenting')">
+    <v-btn icon @click="$emit('toggleCommenting')" :color="commenting ? 'primary' : ''">
       <v-icon>ion-ios-chatboxes</v-icon>
     </v-btn>
-    <span class="mb1 f6">1</span>
+    <span class="mb1 f6">{{ action.comments.length }}</span>
 
     <v-btn icon>
       <v-icon>ion-ios-share-alt</v-icon>
@@ -27,10 +27,13 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { Emit } from "vue-property-decorator";
+import { Emit, Prop } from "vue-property-decorator";
 
 @Component
 export default class PostActions extends Vue {
+  @Prop() action!: object;
+  @Prop() commenting!: boolean;
+
   private kekGiven: boolean = false;
 
   public toggleKek() {
