c89eb5886a93a61f045892a0156905c4618bce15
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-18 03:22:59 +0400

    Now loads data and small adjustments.

--- src/components/Home/Post/Comment/Main.vue
+++ src/components/Home/Post/Comment/Main.vue
@@ -4,10 +4,30 @@
       <CommentAvatar />
 
       <v-list-item-content style="margin-left: -.7em;">
-        <CommentDetails />
+        <CommentDetails :author="comment.author" :date="comment.created" />
 
         <p class="f6 pt0">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, eligendi.
+          <vue-markdown
+            v-if="markdown"
+            :source="comment.content"
+            :class="[
+              $vuetify.theme.dark
+                ? 'white--text br3'
+                : 'grey--text text--darken-4',
+              'fw5'
+            ]"
+          />
+
+          <truncate
+            clamp="Show More"
+            :class="[
+              $vuetify.theme.dark ? 'white--text' : 'grey--text text--darken-4',
+              'fw5'
+            ]"
+            :text="comment.content"
+            :length="500"
+            v-else
+          />
         </p>
 
         <CommentActions />
@@ -20,23 +40,41 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+import { Prop } from "vue-property-decorator";
+import { Comment } from "@/store/post";
+
 import CommentAvatar from "./Avatar.vue";
 import CommentDetails from "./Details.vue";
 import CommentActions from "./Actions.vue";
+import { Getter } from "vuex-class";
+
+/* 
+  Lazy load truncate/markdown because it does need to get imported
+  if it is turned off in the settings.
+*/
+const VueMarkdown = () => import("vue-markdown");
+const truncate = () => import("vue-truncate-collapsed");
 
 @Component({
   components: {
     CommentAvatar,
     CommentDetails,
-    CommentActions
+    CommentActions,
+
+    VueMarkdown,
+    truncate
   }
 })
-export default class CommentMain extends Vue {}
+export default class CommentMain extends Vue {
+  @Prop() comment!: Comment;
+
+  @Getter("markdown", { namespace: "settings" }) private markdown!: boolean;
+}
 </script>
 
 <style lang="scss">
 .comment {
-  margin-left: .3em;
-  margin-right: .75em
+  margin-left: 0.3em;
+  margin-right: 0.75em;
 }
 </style>
