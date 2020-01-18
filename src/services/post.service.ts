03f95925d67e897ce84e4b45250b8fc97cbabd22
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-01-18 09:33:43 -0800

    Added post service.

---
+++ src/services/post.service.ts
@@ -0,0 +1,62 @@
+import ApiService, { backendUrl } from "./api.service";
+
+class PostError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    this.name = this.constructor.name;
+    this.message = message;
+  }
+}
+
+interface CreateData {
+  content: string;
+  image_id: string;
+}
+
+const PostService = {
+  async get(postPublicId: string) {
+    const resource: string = `${backendUrl}/post/get/${postPublicId}`;
+
+    ApiService.setHeader();
+
+    try {
+      const apiResponse = await ApiService.get(resource);
+      return apiResponse.data.post;
+    } catch (error) {
+      throw new PostError(error.response.message);
+    }
+  },
+
+  async create(data: CreateData) {
+    const resource: string = `${backendUrl}/post/create`;
+
+    ApiService.setHeader();
+
+    try {
+      const apiResponse = await ApiService.post(resource, data)
+
+      // Return newly created post.
+      return apiResponse.data.post;
+    } catch (error) {
+      throw new PostError(error.response.message);
+    }
+  },
+
+  async delete(postPublicId: string) {
+    const resource: string = `${backendUrl}/post/delete/${postPublicId}`;
+
+    ApiService.setHeader();
+
+    try {
+      const apiResponse = await ApiService.delete(resource);
+      // Returns true if succeeded.
+      return apiResponse.data.success;
+    } catch (error) {
+      throw new PostError(error.response.message);
+    }
+  }
+}
+
+export default PostService;
+export { PostService, PostError, CreateData };
