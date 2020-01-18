1cedb3148f99de440fa680177fc8561c5ccdd031
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-01-18 10:18:24 -0800

    Added validators and its functions.

--- src/components/Home/ComposerItems/Writer.vue
+++ src/components/Home/ComposerItems/Writer.vue
@@ -1,14 +1,24 @@
 <template>
   <v-window-item>
-    <v-textarea
-      rows="2"
-      auto-grow
-      outlined
-      filled
+    <ValidationObserver ref="observe">
+      <ValidationProvider
+        mode="lazy"
+        rules="required|min:1|max:1600"
+        name="content"
+        v-slot="{ errors }"
+      >
+        <v-textarea
+          rows="2"
+          auto-grow
+          outlined
+          filled
 
-      v-model="content"
-      @keyup="update(content)"
-    ></v-textarea>
+          v-model="content"
+          :error-messages="errors[0]"
+          @keyup="update(content)"
+        ></v-textarea>
+      </ValidationProvider>
+    </ValidationObserver>
   </v-window-item>
 </template>
 
@@ -17,13 +27,35 @@ import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop, Emit } from "vue-property-decorator";
 
-@Component
+import { ValidationProvider } from "vee-validate/dist/vee-validate.full";
+import { ValidationObserver } from "vee-validate";
+
+@Component({
+  components: {
+    ValidationObserver,
+    ValidationProvider
+  }
+})
 export default class Writer extends Vue {
   private content: string = "";
+
+  $refs!: {
+    observe: InstanceType<typeof ValidationObserver>;
+  };
 
   @Emit("update")
   private update(content: string): string {
     return content;
+  }
+
+  public async checkValidity() {
+    const isValid = await this.$refs.observe.validate();
+
+    return isValid;
+  }
+
+  public resetValidator() {
+    this.$refs.observe.reset();
   }
 }
 </script>
