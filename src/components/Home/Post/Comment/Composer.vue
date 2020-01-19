6c1ec623fcb1dbbf841960100f8ec86fbb68fffc
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-01-19 14:00:09 -0800

    Added send button for comment composer.

--- src/components/Home/Post/Comment/Composer.vue
+++ src/components/Home/Post/Comment/Composer.vue
@@ -9,8 +9,11 @@
       filled
     >
       <template v-slot:append>
-        <v-btn icon class="teal mb3 ml2" elevation="2" dark>
+        <v-btn icon class="teal mb3 ml1" elevation="2" dark>
           <v-icon>ion-ios-image</v-icon>
+        </v-btn>
+        <v-btn icon class="teal mb3 ml1" elevation="2" dark>
+          <v-icon>ion-ios-send</v-icon>
         </v-btn>
       </template>
     </v-textarea>
