5cff8827af71ae34a05fb87ff3ea35be62d53415
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-01-25 01:05:28 +0400

    Added updateEditedPost method.

--- src/store/modules/feed/mutations.ts
+++ src/store/modules/feed/mutations.ts
@@ -1,5 +1,6 @@
 import { MutationTree } from "vuex";
 import { FeedState } from "./types";
+import { Post } from "@/store/post";
 
 export const mutations: MutationTree<FeedState> = {
   feedRequest(state) {
@@ -21,6 +22,19 @@ export const mutations: MutationTree<FeedState> = {
     state.requesting = false;
     state.error = true;
     state.feedErrorMsg = errorMsg;
+  },
+
+  updateEditedPost(state, { content, postPublicId }) {
+    const index = state.feedItems.findIndex(
+      // @ts-ignore
+      // Ignore this cause no types need to be specified.
+      post => post.public_id === postPublicId
+    );
+
+    const post: any = state.feedItems[index];
+
+    post.content = content;
+    post.edited = true;
   },
 
   removePost(state, postPublicId: string) {
