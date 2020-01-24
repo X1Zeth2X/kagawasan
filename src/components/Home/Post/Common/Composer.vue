fa966b0ba1f4c68ed105fa5e4111f79cc6d9b4f3
Zeth Leonardo <zeth.leonardo@protonmail.com>
2020-01-24 12:49:01 +0400

    Moved to common.

---
+++ src/components/Home/Post/Common/Composer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="commentComposer" class="mh3 mt3">
+    <v-textarea
+      placeholder="Write a comment..."
+      class="b-card"
+      rows="2"
+      auto-grow
+      solo
+      outlined
+      rounded
+      filled
+    >
+      <template v-slot:append>
+        <v-btn icon class="info mb3 ml1" elevation="2" dark>
+          <v-icon>ion-ios-image</v-icon>
+        </v-btn>
+        <v-btn icon class="teal mb3 ml1" elevation="2" dark>
+          <v-icon>ion-ios-send</v-icon>
+        </v-btn>
+      </template>
+    </v-textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class CommentReplyComposer extends Vue {}
+</script>
