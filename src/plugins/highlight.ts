8186b2f5ec676d0a808b99e1e74e2a0877d11650
Zeth <zeth.leonardo@protonmail.com>
2020-01-29 01:17:10 +0400

    Add more language.

--- src/plugins/highlight.ts
+++ src/plugins/highlight.ts
@@ -7,3 +7,7 @@ import "prismjs/themes/prism-tomorrow.css";
 // Load languages
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-latex";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
