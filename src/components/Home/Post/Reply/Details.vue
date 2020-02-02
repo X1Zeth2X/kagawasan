b8bb05b5e958a7ed0e2fb4b000390f7a7cb1a490
Zeth <zeth.leonardo@protonmail.com>
2020-02-02 17:26:02 +0400

    Added mock reply component.

---
+++ src/components/Home/Post/Reply/Details.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a class="b f6">
+      Johnson Smith
+    </a>
+    &middot;
+
+    <span class="f7 fw5">
+      38 mins ago
+    </span>
+
+    <span class="float-right">
+      <v-btn icon x-small>
+        <v-icon small>
+          ion-ios-arrow-down
+        </v-icon>
+      </v-btn>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class ReplyDetails extends Vue {}
+</script>
---
+++ src/components/Home/Post/Reply/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-list-item three-line class="reply-margin">
+    <v-list-item-avatar size="35">
+      <v-avatar class="primary">
+        <v-icon small>ion-ios-person</v-icon>
+      </v-avatar>
+    </v-list-item-avatar>
+
+    <v-list-item-content
+      :class="$vuetify.breakpoint.mdAndUp ? 'ml-negative' : 'ml2'"
+    >
+      <ReplyDetails />
+
+      <p class="f6 pt0">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur,
+        architecto.
+      </p>
+
+      <ReplyActions />
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import ReplyDetails from "./Details.vue";
+import ReplyActions from "./Actions.vue";
+
+@Component({
+  components: {
+    ReplyDetails,
+    ReplyActions
+  }
+})
+export default class Main extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.reply-margin {
+  margin-left: -1em;
+  margin-right: -1em;
+}
+</style>
