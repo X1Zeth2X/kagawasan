03e2a9afc8991d8bbb6c318f38ac084e00ad19b8
Zeth <zeth.leonardo@protonmail.com>
2020-02-03 20:22:22 +0400

    Added update methods and avatar import.

--- src/components/Home/Post/Reply/Main.vue
+++ src/components/Home/Post/Reply/Main.vue
@@ -1,10 +1,6 @@
 <template>
   <v-list-item three-line class="reply-margin">
-    <v-list-item-avatar size="35">
-      <v-avatar class="primary">
-        <v-icon small>ion-ios-person</v-icon>
-      </v-avatar>
-    </v-list-item-avatar>
+    <Avatar :avatarUrl="null" />
 
     <v-list-item-content
       :class="$vuetify.breakpoint.mdAndUp ? 'ml-negative' : 'ml2'"
@@ -13,18 +9,28 @@
         :author="reply.author"
         :date="reply.created"
         :replyPublicId="reply.public_id"
+        :edited="reply.edited"
         v-on:deleted="$emit('removeReply')"
+        v-on:edit="editing = true"
       />
 
       <p class="f6 pt0">
         <v-scroll-x-transition mode="out-in">
           <Content v-if="!editing" :content="reply.content" />
 
-          <Edit v-else :content="reply.content" />
+          <Edit
+            v-else
+            class="mb-negative-reply"
+            :content="reply.content"
+            v-on:cancel="editing = false"
+            v-on:submit="updateReply"
+          />
         </v-scroll-x-transition>
       </p>
 
       <ReplyActions
+        :action="actionProps"
+        :replyPublicId="reply.public_id"
         v-on:reply="$emit('reply')"
       />
     </v-list-item-content>
@@ -38,14 +44,19 @@ import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
 import { Note } from "@/store/content";
 
+import Avatar from "../Common/Avatar.vue";
 import ReplyDetails from "./Details.vue";
 import ReplyActions from "./Actions.vue";
 
 import Content from "../Common/Content.vue";
+import { Action } from "vuex-class";
 const Edit = () => import("../Common/Edit.vue");
+
+const namespace: string = "reply";
 
 @Component({
   components: {
+    Avatar,
     ReplyDetails,
     ReplyActions,
     Content,
@@ -55,7 +66,32 @@ const Edit = () => import("../Common/Edit.vue");
 export default class Main extends Vue {
   @Prop() reply!: Note;
 
+  @Action("update", { namespace })
+  private updateReplyVuex!: Function;
+
   private editing: boolean = false;
+
+  private actionProps: object = {
+    kekGiven: this.reply.liked,
+    keks: this.reply.likes.length
+  };
+
+  private async updateReply(updatedContent: string) {
+    console.log("Called");
+
+    const isSuccessful: boolean = await this.updateReplyVuex({
+      content: updatedContent,
+      replyPublicId: this.reply.public_id
+    });
+
+    // If update was successful (true).
+    if (isSuccessful) {
+      this.editing = false;
+
+      this.reply.content = updatedContent;
+      this.reply.edited = true;
+    }
+  }
 }
 </script>
 
