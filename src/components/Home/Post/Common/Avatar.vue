a864d5dc182ec208024e0b703d11a8a14d6f0159
Zeth <zeth.leonardo@protonmail.com>
2020-02-03 20:21:20 +0400

    Updated avatar imports.

---
+++ src/components/Home/Post/Common/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list-item-avatar class="pb1 ml2">
+    <a>
+      <v-avatar size="40">
+        <v-img
+          src="https://api.adorable.io/avatars/50/abott@adorable.png"
+          v-if="avatarUrl"
+        ></v-img>
+        <v-icon dark class="primary" v-else>ion-ios-person</v-icon>
+      </v-avatar>
+    </a>
+  </v-list-item-avatar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Component
+export default class Avatar extends Vue {
+  @Prop() avatarUrl!: string | null;
+}
+</script>
