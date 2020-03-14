07d323e7d0bf9544685de6ba3e0e325e56ef6d7d
Zeth <zeth.leonardo@protonmail.com>
2020-03-15 00:00:26 +0400

    Renamed from Minimal.

--- src/components/Settings/Layout.vue
+++ src/components/Settings/Layout.vue
@@ -68,8 +68,8 @@ export default class Layout extends Vue {
       onChange: this.toggleMiniDrawer
     },
     {
-      title: "Minimal",
-      subtitle: "Hide right column on home page.",
+      title: "Full Mode",
+      subtitle: "Show right column on home page.",
       value: this.$store.getters["settings/rightContent"],
       onChange: this.toggleRightContent
     }
